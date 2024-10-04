03be26a06fac673db1b1b55e2a354fdfdb55b6a0
Javad Babaei <javadbabaei009@gmail.com>
2024-10-04 19:50:24 +0330

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
 "use client";
 
-import { useRef } from "react";
-
 export default function Home() {
-  const observer = useRef(null);
-
   return (
     <main className="flex min-h-screen flex-col gap-4 p-6">
       <h1 className="text-purple-500 font-bold text-3xl">
         Intersection Observer Infinity Scroll
       </h1>
       <span>
-      Lorem ipsum odor amet, consectetuer adipiscing elit. Convallis blandit consequat nibh cursus tortor lacinia sapien. Euismod ornare rhoncus, malesuada pellentesque est pharetra erat gravida. Odio erat nisl dui accumsan pretium etiam in in. Nostra rhoncus sociosqu metus nisi ad adipiscing? Elit nullam maximus porttitor velit taciti. Dictum proin sed per lacinia ad. Litora augue fusce eros in congue. Faucibus mollis parturient sem orci hac. Viverra vehicula integer aliquam iaculis velit vehicula montes luctus fusce.
+        Lorem ipsum odor amet, consectetuer adipiscing elit. Convallis blandit
+        consequat nibh cursus tortor lacinia sapien. Euismod ornare rhoncus,
+        malesuada pellentesque est pharetra erat gravida. Odio erat nisl dui
+        accumsan pretium etiam in in. Nostra rhoncus sociosqu metus nisi ad
+        adipiscing? Elit nullam maximus porttitor velit taciti. Dictum proin sed
+        per lacinia ad. Litora augue fusce eros in congue. Faucibus mollis
+        parturient sem orci hac. Viverra vehicula integer aliquam iaculis velit
+        vehicula montes luctus fusce.
       </span>
       <div className="grid grid-cols-5 gap-4 mb-4">
         {Array.from({ length: 30 }).map((_, index) => (
